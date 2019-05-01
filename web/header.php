08316feffa666c5623afec43bb18485dbc167c64
Etienne Beaulac <etiennebeaulac@gmail.com>
2019-05-01 10:30:02 -0600

    clean up

--- web/header.php
+++ web/header.php
@@ -3,13 +3,11 @@
 <nav class="navbar navbar-inverse">
   <div class="container-fluid">
     <div class="navbar-header">
-      <a class="navbar-brand" href="#">Entertainement 720</a>
+      <a class="navbar-brand" href="#">Etienne Beaulac</a>
     </div>
     <ul class="nav navbar-nav">
-      <li class="<?php echo ($page == "home" ? "active" : "")?>"><a href="./home.php">Home</a></li>
-      <li class="<?php echo ($page == "about-us" ? "active" : "")?>"><a href="./about-us.php">About Us</a></li>
-      <li class="<?php echo ($page == "login" ? "active" : "")?>"><a href="./login.php">Login</a></li>
-      <li><a href="./home.php">Logout</a></li>
+      <li class="<?php echo ($page == "home" ? "active" : "")?>"><a href="./index.php">About Me</a></li>
+      <li class="<?php echo ($page == "assignments" ? "active" : "")?>"><a href="./assignments.php">Assignments</a></li>
     </ul>
   </div>
 </nav>
