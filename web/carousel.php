fc9a6acaca93a604f9b0efa4824930b08d636a05
Etienne Beaulac <etiennebeaulac@gmail.com>
2019-05-01 11:07:50 -0600

    change size of carousel

--- web/carousel.php
+++ web/carousel.php
@@ -8,21 +8,21 @@
     <div class="carousel-inner">
       <div class="carousel-item active">
         <img src="https://scontent-sjc3-1.xx.fbcdn.net/v/t1.0-9/56679255_10156427818064001_2327780044304285696_n.jpg?_nc_cat=103&_nc_ht=scontent-sjc3-1.xx&oh=eeedfd5e17ef608071c18de63e0ee330&oe=5D304342" class="d-block w-100" alt="stela & etienne">
-        <div class="carousel-caption d-none d-md-block">
+        <div class="carousel-caption d-none d-md-block w-100">
           <h5>My wife, Stela, and I</h5>
           <p>A picture of us when we went to Quebec for spring break</p>
         </div>
       </div>
       <div class="carousel-item">
         <img src="https://scontent-sjc3-1.xx.fbcdn.net/v/t1.0-9/53581858_10210755041271413_7728726085021466624_o.jpg?_nc_cat=111&_nc_ht=scontent-sjc3-1.xx&oh=bacb8ce78ba74165a23657b378d2ac3e&oe=5D7008E1" class="d-block w-100" alt="etienne & kiyomi">
-        <div class="carousel-caption d-none d-md-block">
+        <div class="carousel-caption d-none d-md-block w-100">
           <h5>My daughter and I</h5>
           <p>A picture taken at work of my daughter, Kiyomi, and I</p>
         </div>
       </div>
       <div class="carousel-item">
         <img src="https://scontent-sjc3-1.xx.fbcdn.net/v/t1.0-9/50970775_10210552012235814_8501465103290335232_n.jpg?_nc_cat=106&_nc_ht=scontent-sjc3-1.xx&oh=c7373d570f4273f78482441b1b1fe3ce&oe=5D2AE9D3" class="d-block w-100" alt="kiyomi after bath">
-        <div class="carousel-caption d-none d-md-block">
+        <div class="carousel-caption d-none d-md-block w-100">
           <h5>Bath time!</h5>
           <p>Kiyomi after a bath</p>
         </div>
