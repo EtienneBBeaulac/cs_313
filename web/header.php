3cdb13fadf5f0855b4b538d1fac10320651c10e4
Etienne Beaulac <etiennebeaulac@gmail.com>
2019-05-01 10:53:36 -0600

    add external css

--- web/header.php
+++ web/header.php
@@ -1,3 +1,5 @@
+<link rel="stylesheet" href="style.css">
+
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
   <a class="navbar-brand" href="#">Etienne Beaulac</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
