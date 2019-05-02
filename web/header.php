332e222aeee35ca1a4bdabb3ee70862928e29b0c
Etienne Beaulac <etiennebeaulac@gmail.com>
2019-05-01 22:36:00 -0600

    add text

--- web/header.php
+++ web/header.php
@@ -1,5 +1,5 @@
 <link rel="stylesheet" href="style.css">
-
+<?php date_default_timezone_set("America/Boise"); ?>
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
   <a class="navbar-brand" href="#">Etienne Beaulac</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
