0c7416b189591c6ea88b28887dc8dd2e008a9948
Etienne Beaulac <etiennebeaulac@gmail.com>
2019-05-01 22:27:54 -0600

    add text

--- web/index.php
+++ web/index.php
@@ -26,7 +26,7 @@
   </div>
   <?php 
     date_default_timezone_set("America/Boise");
-    echo "Today is " . date("Y.m.d");
+    echo "Today is " . date("DD MM YYYY");
     require "bootstrap-includes.php";
   ?>
 </body>
