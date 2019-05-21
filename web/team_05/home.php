36b5bd45b35a11da407db1d469283e3a6e00d646
Etienne Beaulac <etiennebeaulac@gmail.com>
2019-05-21 15:32:15 -0600

    team 05

---
+++ web/team_05/home.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Team 05</title>
+</head>
+
+<body>
+  <h1>Scripture Resources</h1>
+<?php 
+include 'database-connection.php';
+foreach ($db->query('SELECT book, chapter, verse, content FROM other.scripture') as $item) {
+  echo '{$item["book"]} {$item["chapter"]}:{$item["verse"] - {$item["content"]}<br>';
+}
+?>
+
+</body>
+
+</html>
