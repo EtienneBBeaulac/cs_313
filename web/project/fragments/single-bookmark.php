5def914a3bf8669aaeb1748837fd8e1fb1a59377
Etienne Beaulac <etiennebeaulac@gmail.com>
2019-05-21 12:55:28 -0600

    display db stuff

--- web/project/fragments/single-bookmark.php
+++ web/project/fragments/single-bookmark.php
@@ -1,10 +1,10 @@
-<a class="list-group-item" href="<?php echo $bm['url'] ?>">
+<a class="list-group-item" href="<?php echo $bm['bookmark_url'] ?>">
   <div class="row">
     <div class="col-md-4">
-      <h6 class="my-0"><?php echo $bm['name']; ?></h6>
+      <h6 class="my-0"><?php echo $bm['bookmark_name']; ?></h6>
     </div>
     <div class="col-md-8">
-      <span><?php echo $bm['url'] ?></span>
+      <span><?php echo $bm['bookmark_url'] ?></span>
     </div>
   </div>
 </a>
