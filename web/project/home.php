bcad90c2aa15e0e6cb14acb7510cf86dd7a3a38f
Etienne Beaulac <etiennebeaulac@gmail.com>
2019-05-22 14:14:17 -0600

    sign up form project

--- web/project/home.php
+++ web/project/home.php
@@ -19,17 +19,21 @@ if (!isset($_SESSION['login'])) {
 <body>
   <?php include 'navbar.php';
   include 'database-connection.php'; ?>
-<div class="container bookmarks-container">
-  <?php
-  // $bookmarks = [['name' => 'bookmark1', 'url' => 'http://www.google.com'], ['name' => 'bookmark2', 'url' => 'http://www.amazon.com']];
-  $stmt = $db->prepare('SELECT bookmark_name, bookmark_url FROM user_bookmark WHERE user_id=:user_id');
-  $stmt->execute(array(':user_id' => $_SESSION['login']['id']));
-  $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
-  foreach ($rows as $bm) {
-    include 'fragments/single-bookmark.php';
-  }
-  ?>
-</div>
+  <div class="container bookmarks-container">
+    <?php
+    // $bookmarks = [['name' => 'bookmark1', 'url' => 'http://www.google.com'], ['name' => 'bookmark2', 'url' => 'http://www.amazon.com']];
+    $stmt = $db->prepare('SELECT bookmark_name, bookmark_url FROM user_bookmark WHERE user_id=:user_id');
+    $stmt->execute(array(':user_id' => $_SESSION['login']['id']));
+    $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    if (!empty($rows)) {
+      foreach ($rows as $bm) {
+        include 'fragments/single-bookmark.php';
+      }
+    } else {
+      echo "You have no bookmarks";
+    }
+    ?>
+  </div>
   <?php include 'bootstrap-bottom.php' ?>
 </body>
 
