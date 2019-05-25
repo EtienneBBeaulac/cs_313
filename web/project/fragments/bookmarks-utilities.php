3bd065a459783ed0ab6302b85ac0b558ee2b0afb
Etienne Beaulac <etiennebeaulac@gmail.com>
2019-05-24 22:22:31 -0600

    search bookmarks

--- web/project/fragments/bookmarks-utilities.php
+++ web/project/fragments/bookmarks-utilities.php
@@ -1,6 +1,10 @@
-<form class="input-group mb-3" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="get">
-  <input type="text" class="form-control" placeholder="Search...">
-  <div class="input-group-append">
-    <button class="btn btn-outline-secondary" type="submit">Search</button>
-  </div>
-</form>
+<div class="row">
+    <div class="col-md-4">
+        <form class="input-group mb-3" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="get">
+          <input type="text" name="search" class="form-control" placeholder="Search...">
+          <div class="input-group-append">
+            <button class="btn btn-outline-secondary" type="submit">Search</button>
+          </div>
+        </form>
+    </div>
+</div>
