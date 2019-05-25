6e13a6bdbe99ded0247fc298fc96f3d228a627d5
Etienne Beaulac <etiennebeaulac@gmail.com>
2019-05-24 23:53:59 -0600

    search bookmarks

--- web/project/home.php
+++ web/project/home.php
@@ -4,6 +4,23 @@ session_start();
 if (!isset($_SESSION['login'])) {
   header('Location: logout.php');
 }
+
+function matchesSearch($bm, $search) {
+  $bm = strtoupper($bm);
+  $search = strtoupper($search);
+  
+  similar_text($bm, $search, $percent);
+  if ($percent > 75) {
+    return true;
+  }
+
+  similar_text($search, $bm, $percent);
+  if ($percent > 75) {
+    return true;
+  }
+
+  return false;
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -21,20 +38,21 @@ if (!isset($_SESSION['login'])) {
   require 'database-connection.php'; ?>
   <div class="container bookmarks-container">
     <?php
-    if (!isset($_GET['search'])) {
-      $stmt = $db->prepare('SELECT bookmark_name, bookmark_url FROM user_bookmark WHERE user_id=:user_id');
-      $stmt->execute(array(':user_id' => $_SESSION['login']['id']));
-      $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
-      if (!empty($rows)) {
-        require 'fragments/bookmarks-utilities.php';
-        echo '<div class="container">';
-        foreach ($rows as $bm) {
+    require 'fragments/bookmarks-utilities.php';
+    $stmt = $db->prepare('SELECT bookmark_name, bookmark_url FROM user_bookmark WHERE user_id=:user_id');
+    $stmt->execute(array(':user_id' => $_SESSION['login']['id']));
+    $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    if (!empty($rows)) {
+      $search = isset($_GET['search']) ? test_input($_GET['search']) : '';
+      echo '<div class="container">';
+      foreach ($rows as $bm) {
+        if (matchesSearch($bm, $search)) {
           require 'fragments/single-bookmark.php';
         }
-        echo '</div>';
-      } else {
-        echo "You have no bookmarks";
       }
+      echo '</div>';
+    } else {
+      echo "You have no bookmarks";
     }
     ?>
   </div>
